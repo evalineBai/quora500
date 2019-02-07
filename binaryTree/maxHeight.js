b64372088cbbaba43966d24fb0eb2f1ce721b6d3
Evaline Bai <ebai@blueprintpower.com>
2019-02-07 09:12:28 -0500

    Add binary tree repo

---
+++ binaryTree/maxHeight.js
@@ -0,0 +1,6 @@
+// https://www.techiedelight.com/calculate-height-binary-tree-iterative-recursive/
+// Calculate height of binary tree
+// Write an efficient algorithm to compute the height of a binary tree.
+// The height or depth of a tree is number or edges or nodes on longest path form root to leaf.
+// Height of empty tree is 0 and height of tree with one node is 1.
+
